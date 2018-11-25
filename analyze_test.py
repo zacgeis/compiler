971df58308f56ddc2681d8f3bc4aa037d3e26a63
zacgeis <zacgeis@gmail.com>
2018-11-25 15:48:44 -0800

    Start analyze test

---
+++ analyze_test.py
@@ -0,0 +1,27 @@
+import unittest
+from parse import *
+from lex import *
+from analyze import *
+
+class TestAnalyze(unittest.TestCase):
+    def testBasic(self):
+        originalString = """
+        myNum: int = 1;
+        func myFunction(x: int): int {
+            myLocal: int = 0;
+            if x == myNum {
+                myLocal = 2;
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        """
+        lexxer = Lexxer(originalString)
+        tokens = lexxer.lex()
+        parser = Parser(tokens, originalString)
+        nodes = parser.parse()
+        self.assertEqual(True, True)
+
+if __name__ == "__main__":
+    unittest.main()
